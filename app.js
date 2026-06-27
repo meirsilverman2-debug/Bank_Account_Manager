ca8a428b5fa337a5269bfc6b6af00371a3168f30
Meir925man <meirsilverman2@gmail.com>
2026-06-27 23:29:04 +0300

    bank account finshed project without all of the requirements yet

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import rl from "readline-sync"
-import { CreateCustomer, ShowCustomers, Deposit, withdraw, searchCustomer} from "./system_function.js"
+import { CreateCustomer, ShowCustomers, Deposit, withdraw, searchCustomer, closeAccount, showStatistics } from "./system_function.js"
 
-const bankAccounts = []
+const bankAccounts = [];
 
 
 function Bank() {
     let choice;
     const options = {
         menu: function () {
-            const menu = "option-1: create_customer"
-            return menu
+            const menu = "option-1: create_customer";
+            return menu;
         },
         add: this.menu
     }
@@ -19,41 +19,50 @@ function Bank() {
 let choice;
 
 do {
-    const menu = "option-1: Create Customer\noption-2: Show Customers\noption-3: Deposit\noption-4: Withdraw\noption-5: Search Customer";
+    const menu = "option-1: Create Customer\noption-2: Show Customers\noption-3: Deposit\noption-4: Withdraw\noption-5: Search Customer\noption-6: Close Account\noption-7: Show Statistics";
     console.log(menu);
     choice = rl.question("Please enter your choice: ");
     if (choice === "1") {
 
-        const name = rl.question("Enter a name: ")
-        const accountType = rl.question("Enter a account type choose: (regular, premium, student): ")
+        const name = rl.question("Enter a name: ");
+        const accountType = rl.question("Enter a account type choose: (regular, premium, student): ");
         const amount = rl.questionInt("Enter an amount: ");
 
         console.log(CreateCustomer(name, accountType, amount));
+
     } else {
         if (choice === "2") {
-            ShowCustomers()
+            ShowCustomers();
         } else {
             if (choice === "3") {
-                const CustomerID = rl.questionInt("Please enter an ID: ")
-                const depositAmount = rl.questionInt("Please enter the amount: ")
+                const CustomerID = rl.questionInt("Please enter an ID: ");
+                const depositAmount = rl.questionInt("Please enter the amount: ");
                 console.log(Deposit(CustomerID, depositAmount));
-            }else {
+            } else {
                 if (choice === "4") {
-                const CustomerID = rl.questionInt("Please enter an ID: ")
-                const withdrawAmount = rl.questionInt("Please enter the amount: ")
-                console.log(withdraw(CustomerID, withdrawAmount));
+                    const CustomerID = rl.questionInt("Please enter an ID: ");
+                    const withdrawAmount = rl.questionInt("Please enter the amount: ");
+                    console.log(withdraw(CustomerID, withdrawAmount));
 
-                }else {
-                    if (choice === "5"){
-                        const customerID = rl.questionInt("Please enter an id to search for: ")
-                        const name = rl.question("Please enter a name: ")
-                        console.log(searchCustomer(customerID, name))
+                } else {
+                    if (choice === "5") {
+                        const customerID = rl.questionInt("Please enter an id to search for: ");
+                        console.log(searchCustomer(customerID));
+                    } else {
+                        if (choice === "6") {
+                            const customerID = rl.questionInt("Please enter an id for account you want to deactivate: ");
+                            console.log(closeAccount(customerID));
+                        } else {
+                            if (choice === "7") {
+                                console.log(showStatistics());
+                            }
+                        }
                     }
                 }
             }
         }
     }
-} while (choice !== "stop")
+} while (choice !== "stop");
 
 
 
--- system_function.js
+++ system_function.js
@@ -16,7 +16,7 @@ function genretAccountID() {
 
 const bankIdManager = genretAccountID()
 
-
+// 1. Create Customer:
 export function CreateCustomer(name, accountType, balance = 0) {
     const bankAcount = {}
     if (!name || typeof (name) !== "string") {
@@ -45,23 +45,29 @@ export function CreateCustomer(name, accountType, balance = 0) {
             }
         }
     }
-    // return "Customer created successfully"
+    return "Customer created successfully"
 }
 
+
+// 2. Show Customers:
 export function ShowCustomers() {
     console.log(bankAccounts)
 }
 
+
+// 3. Deposit:
 export function Deposit(customerID, amount) {
+    let response = `deposit ${amount} has entered into the account with the id ${customerID}`;
+
     bankAccounts.forEach((bankAcount) => {
         if (bankAcount.id !== customerID) {
-            return `We dont have an ID ${customerID} in our system!`
+            response = `We dont have an ID ${customerID} in our system!`
         } else {
             if (bankAcount.isActive !== true) {
-                return `This account is not active so you cannot deposit to it!`
+                response = `This account is not active so you cannot deposit to it!`
             } else {
                 if (!amount > 0) {
-                    return `A deposit cannot be negative!`
+                    response = `A deposit cannot be negative!`
                 } else {
                     bankAcount.balance += amount
 
@@ -70,23 +76,25 @@ export function Deposit(customerID, amount) {
             }
         }
     })
-    return `deposit ${amount} has entered into the account with the id ${customerID}`;
+    return response;
 }
 
 
+// 4. Withdraw:
 export function withdraw(customerID, amount) {
+    let response = `This ${amount} has been withdraw account with the id ${customerID}`;
     bankAccounts.forEach((bankAcount) => {
         if (bankAcount.id !== customerID) {
-            return `We dont have an ID ${customerID} in our system!`
+            response = `We dont have an ID ${customerID} in our system!`
         } else {
             if (bankAcount.isActive !== true) {
-                return `This account is not active so you cannot withdraw from it!`
+                response = `This account is not active so you cannot withdraw from it!`
             } else {
                 if (!amount > 0) {
-                    return `A withdraw cannot be negative!`
+                    response = `A withdraw cannot be negative!`
                 } else {
                     if (!bankAccounts.balance >= amount) {
-                        return `You cannot withdraw more than you already have in your bankaccount`
+                        response = `You cannot withdraw more than you already have in your bankaccount`
                     }
                     bankAcount.balance -= amount
 
@@ -96,38 +104,34 @@ export function withdraw(customerID, amount) {
         }
     })
     // return `Withdraw completed successfully`;
-    return `This ${amount} has been withdraw account with the id ${customerID}`;
+    return response
 
 }
 
-let bankAccounts1 = [{ id: 1, name: "moshe" }, { id: 2, name: "mimi" }, { id: 3, name: "miri" }]
 
-export function searchCustomer(customerID = 0, name=null) {
-    let account = 0;
-    if (!name && !customerID) {
-        return `You did not anter something to cearch for`
-    } else {
-        bankAccounts1.forEach((bankAccount) => {
-            if (bankAccount.id === customerID || bankAccount.name.toLowerCase() === name) {
-                account = bankAccount;
-            }
-        })
-    }
+// For testing
+// let bankAccounts1 = [{ id: 1, name: "moshe" }, { id: 2, name: "mimi" }, { id: 3, name: "miri" }]
 
-    if (account) {
-        return account
+
+// 5. Search Customer:
+export function searchCustomer(customerID = 0) {
+
+    let account = null
+    bankAccounts.forEach((bankAccount) => {
+        if (bankAccount.id === customerID) {
+            account = bankAccount
+        }
+    })
+
+    if (account === null) {
+        account = `there is not any account with the ID ${customerID}`
     }
-    return `${customerID} or ${name} name does not exsits in our system!`
+    return account
 }
 
-
-const result = searchCustomer("mimi")
-console.log(result)
-
-
-
-
-
+// This is also for testing
+// const result = searchCustomer()
+// console.log(result)
 
 
 // CreateCustomer("Meir", "Regular", 100)
@@ -135,8 +139,55 @@ console.log(result)
 
 
 
+// 6. Close Account:
+export function closeAccount(id) {
+    let isexsits = false;
+    bankAccounts.forEach((bankAcount) => {
+        if (id === bankAcount.id) {
+            bankAcount.isActive = false;
+            isexsits = true;
+        }
+    })
+
+    if (isexsits) {
+        return `Account closed successfully`;
+    } else {
+        return `This account does not exsits in our system`
+    }
+
+}
 
 
+// 7. Show Statistics:
+
+export function showStatistics() {
+
+    const statistics = bankAccounts.reduce((statistics, bankAccount) => {
+        statistics.Total_Customers = bankAccounts.length;
+
+        if (bankAccount.isActive) {
+            statistics.Active_Accounts += 1;
+        }
+
+        statistics.Total_Money += bankAccount.balance;
+        statistics.Average_Balance = statistics.Total_Money / statistics.Total_Customers;
+
+        return statistics;
+
+    }, {
+        Total_Customers: 0,
+        Active_Accounts: 0,
+        Total_Money: 0,
+        Average_Balance: 0,
+        Highest_Balance: 0
+    })
+
+    let bankAccounts2 = [...bankAccounts].sort((a, b) => a.balance - b.balance);
+    statistics.Highest_Balance = bankAccounts2.pop(bankAccounts2.balance);
+
+
+    return statistics;
+}
 
 
 
